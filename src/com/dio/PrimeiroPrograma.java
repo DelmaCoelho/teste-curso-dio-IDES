258915e37c9db155fc3eefb24642bea4495a1bc3
DelmaCoelho <delmaparecidab@gmail.com>
2021-11-01 20:53:12 -0300

    "first commit"

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+		public static  void main(String[] args) {
+		Livro livro = new livro();
+		Gato gato = new gato();
+		
+		System.out.println(gato);
+		System.out.println(livro);    
+		
+		class gato {
+			public String gato;
+		class livro {
+			public String livro;		
+		}
+		
+}		
+
+
+}
+}
+
+
+
